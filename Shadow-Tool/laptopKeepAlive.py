b055f7c4201493b1a4d2d2cd95d1e7b7063747a7
Shadowdrums <122622018+Shadowdrums@users.noreply.github.com>
2023-02-14 08:40:41 -0500

    Update laptopKeepAlive.py

--- Shadow-Tool/laptopKeepAlive.py
+++ Shadow-Tool/laptopKeepAlive.py
@@ -3,6 +3,6 @@ import os
 
 os.system("@ECHO OFF)
 os.system("cd %TEMP%")
-os.system("powershell -command Powercfg SETACVALUEINDEX 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 000")
-os.system("powerhsell -command Powercfg SETDCVALUEINDEX 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 000")
+os.system("powershell -command powercfg -setacvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0")
+os.system("powerhsell -command powercfg -setdcvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0")
 os.system(powerhsell -command exit")
