c6e0bc9299248d9626dcce91df9272e205662af7
Shadowdrums <122622018+Shadowdrums@users.noreply.github.com>
2023-02-27 05:50:34 -0500

    Update Sniffer.py

--- Shadow-Tool/Sniffer.py
+++ Shadow-Tool/Sniffer.py
@@ -1,34 +1,34 @@
 from scapy.all import *
-import scapy.all as scapy
 import time
 
-target_ip = "FF.FF.FF.1/24"
+target_ip = get_if_addr(conf.iface) # Use host IP as target
+print("Monitoring network for suspicious traffic...")
 
 # Open output file
 with open("suspicious_traffic.txt", "a") as f:
     # Start an infinite loop
     while True:
+        # Define packet handler function
         def detect_packet(pkt):
-            if pkt.haslayer(scapy.TCP) and pkt.haslayer(Raw) and b"MS_T120" in pkt[Raw].load:
-                print("[+] Remote Desktop connection detected from " + pkt[scapy.IP].src + ":" + str(pkt[scapy.TCP].sport))
-                f.write("[+] Remote Desktop connection detected from " + pkt[scapy.IP].src + ":" + str(pkt[scapy.TCP].sport) + "\n")
-            elif pkt.haslayer(scapy.ICMP) and pkt.getlayer(scapy.ICMP).type == 8:
-                print("[+] Ping detected from " + pkt[scapy.IP].src)
-                f.write("[+] Ping detected from " + pkt[scapy.IP].src + "\n")
-            elif pkt.haslayer(scapy.TCP) and pkt.getlayer(scapy.TCP).flags == "S":
-                print("[+] SYN packet detected from " + pkt[scapy.IP].src + ":" + str(pkt[scapy.TCP].sport))
-                f.write("[+] SYN packet detected from " + pkt[scapy.IP].src + ":" + str(pkt[scapy.TCP].sport) + "\n")
-            elif pkt.haslayer(scapy.IP) and pkt.haslayer(scapy.TCP) and pkt[scapy.IP].dst == "ATTACKER_IP_ADDRESS_OR_DOMAIN":
-                print("[+] Outbound connection detected to attacker from " + pkt[scapy.IP].src + ":" + str(pkt[scapy.TCP].sport))
-                f.write("[+] Outbound connection detected to attacker from " + pkt[scapy.IP].src + ":" + str(pkt[scapy.TCP].sport) + "\n")
-            elif pkt.haslayer(scapy.DNS) and len(pkt[scapy.DNSQR].qname) > 50:
-                print("[+] Possible DNS tunneling detected from " + pkt[scapy.IP].src + " to " + pkt[scapy.DNSQR].qname.decode())
-                f.write("[+] Possible DNS tunneling detected from " + pkt[scapy.IP].src + " to " + pkt[scapy.DNSQR].qname.decode() + "\n")
-            elif pkt.haslayer(scapy.ARP) and pkt[scapy.ARP].op == 2:
-                print("[+] Possible ARP spoofing detected from " + pkt[scapy.IP].src + " to " + pkt[scapy.ARP].psrc)
-                f.write("[+] Possible ARP spoofing detected from " + pkt[scapy.IP].src + " to " + pkt[scapy.ARP].psrc + "\n")
-            elif pkt.haslayer(scapy.TCP) and pkt.haslayer(Raw) and (b"USER" in pkt[Raw].load or b"PASS" in pkt[Raw].load):
-                print("[+] FTP command detected from " + pkt[scapy.IP].src + ": " + pkt[Raw].load.decode().strip())
-                f.write("[+] Possible FTP command detected from " + pkt[scapy.IP].src + "\n")
-
-        sniff(filter="tcp or icmp or udp or arp", prn=detect_packet, store=0)
+            if pkt.haslayer(TCP) and pkt.haslayer(Raw) and b"MS_T120" in pkt[Raw].load:
+                message = f"[+] Remote Desktop connection detected from {pkt[IP].src}:{pkt[TCP].sport}"
+                print(message)
+                f.write(message + "\n")
+            
+            if pkt.haslayer(ICMP) and pkt.getlayer(ICMP).type == 8:
+                message = f"[+] Ping detected from {pkt[IP].src}"
+                print(message)
+                f.write(message + "\n")
+            
+            if pkt.haslayer(TCP) and pkt.getlayer(TCP).flags == "S":
+                message = f"[+] SYN packet detected from {pkt[IP].src}:{pkt[TCP].sport}"
+                print(message)
+                f.write(message + "\n")
+            
+            if pkt.haslayer(IP) and pkt.haslayer(TCP) and pkt[IP].dst == target_ip:
+                message = f"[+] Outbound connection detected to {target_ip} from {pkt[IP].src}:{pkt[TCP].sport}"
+                print(message)
+                f.write(message + "\n")
+        
+        # Use sniff() function to capture and handle packets
+        sniff(filter="tcp or icmp", prn=detect_packet, store=0, timeout=10, iface=conf.iface)
